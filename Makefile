2f3873a633b2f915b5134956c52dde5bef52aa78
natekimball <natekimball1@gmail.com>
2023-11-29 23:31:59 -0500

    updates

--- Makefile
+++ Makefile
@@ -1,10 +1,19 @@
 sync:
-	rsync -avz Makefile mario.py DQNAgent.py requirements.txt train.slurm tma5gv@rivanna:/scratch/tma5gv/SuperMarioRL
+	-rsync -avz Makefile mario.py DQNAgent.py requirements.txt train.slurm tma5gv@rivanna:/scratch/tma5gv/SuperMarioRL
+	
+# rsync -ravz --exclude=*.sif --exclude=blob tma5gv@rivanna:/scratch/tma5gv/SuperMarioRL/images .
+
+get-results:
+	-rsync -ravz tma5gv@rivanna:/scratch/tma5gv/SuperMarioRL/out .
+
+get-model:
+	-rsync -ravz tma5gv@rivanna:/scratch/tma5gv/SuperMarioRL/models .
 
 run:
 	time singularity exec --bind `pwd`:/home --pwd /home --nv images/gym.sif python mario.py
 
 # rsync -avz --exclude='MAR' ./ tma5gv@rivanna:/scratch/tma5gv/super-mario-RL
+
 
 # source ~/anaconda/bin/activate
 # conda activate MAR
--- mario.py
+++ mario.py
@@ -5,44 +5,89 @@ import gym
 from DQNAgent import DQNAgent
 import numpy as np
 import matplotlib.pyplot as plt
-
-env = gym.make('SuperMarioBros-v3', apply_api_compatibility=True)#, render_mode="human")
+from tqdm import tqdm
+import argparse
+    
+env = gym.make('SuperMarioBros-v2', apply_api_compatibility=True)#, render_mode="human")
 env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 state_shape = env.observation_space.shape
 action_size = env.action_space.n
-agent = DQNAgent(state_shape, action_size)
-TIMESTEPS = 1000000
-batch_size = 32
 
-high_scores = []
-record = 0
-done = True
-for step in range(TIMESTEPS):
-    if step % 100 == 0:
-        high_scores.append(record)
-    if done:
-        state, info = env.reset()
-    action = agent.act(state)
-    next_state, reward, terminated, truncated, info = env.step(action)
-    record = max(record, info['score'])
-    done = terminated or truncated
-    agent.remember(state, action, reward, next_state, done)
-    state = next_state
-    if len(agent.memory) > batch_size:
-        agent.replay(batch_size)
+def train(agent, batch_size=32, timesteps=1000000, render=False, save_dir='models/mario'):
+    high_scores = []
+    record = 0
+    done = True
+    for step in tqdm(range(timesteps)):
+        if step % 100 == 0:
+            high_scores.append(record)
+            if step%500 == 0:
+                plot(high_scores)
+        if done:
+            state, info = env.reset()
+        action = agent.act(state)
+        next_state, reward, terminated, truncated, info = env.step(action)
+        if render:
+            env.render()
+        record = max(record, info['score'])
+        done = terminated or truncated
+        agent.remember(state, action, reward, next_state, done)
+        state = next_state
+        if len(agent.memory) > batch_size:
+            agent.replay(batch_size)
+        
+    env.close()
+    agent.save(save_dir)
+    plot(high_scores)
+
+def plot(high_scores):
+    plt.figure(figsize=(10, 5))
+    plt.plot(high_scores)
+    plt.xlabel('Every 100th time step')
+    plt.ylabel('High Score')
+    plt.savefig('out/mario_scores.png')
+    plt.close()
     
-env.close()
-agent.save('mario-v1')
+def test(agent, render=True):
+    done = True
+    high_score = 0
+    for step in tqdm(range(1000)):
+        if done:
+            state, info = env.reset()
+        action = agent.act(state)
+        state, reward, terminated, truncated, info = env.step(action)
+        if render:
+            env.render()
+        done = terminated or truncated
+        high_score = max(high_score, info['score'])
+    env.close()
+    print("High score: ", high_score)
 
-plt.figure(figsize=(10, 5))
-plt.plot(high_scores, label='Highest Score at time step')
-plt.xlabel('Every 100th time step')
-plt.ylabel('Score')
-plt.legend()
-plt.savefig('mario_scores.png')
-plt.show()
+def parse_arguments():
+    parser = argparse.ArgumentParser(description='Super Mario Bros DQN')
+    parser.add_argument('--time', type=int, help='Number of timesteps for training')
+    parser.add_argument('--batch_size', type=int, default=32, help='Batch size for training')
+    parser.add_argument('--load_model', type=str, help='Path to the model directory')
+    parser.add_argument('--test', action='store_true', help='Enable test mode')
+    parser.add_argument('--render', action='store_true', help='Enable rendering')
+    parser.add_argument('--save_dir', type=str, default='models/mario', help='Directory to save model to')
+    return parser.parse_args()
 
+def main():
+    args = parse_arguments()
+    if args.load_model:
+        agent = DQNAgent(state_shape, action_size, load_model=args.load_model)
+    else:
+        agent = DQNAgent(state_shape, action_size)
+    if args.test:
+        agent.test_mode = True
+        test(agent, render=args.render)
+    else:
+        train(agent, batch_size=args.batch_size, timesteps=args.time, render=args.render, save_dir=args.save_dir)
+
+if __name__ == '__main__':
+    main()
+        
 # # EPISODES = 1
 # # scores = []
 # # moving_avg_scores = []
